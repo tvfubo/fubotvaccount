f3ff33c167909292ab0a8b7d2934cef3f011c409
tvfubo <princemiso@outlook.com>
2026-08-20 16:59:19 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,15 +12,13 @@ export default defineConfig({
 				{
 					tag: 'meta',
 					attrs: {
-						name: 'google-site-verification',
-						content: 'tUI9BT1RIABRmftik1nEVColyPRItHU67IwjWOKTD74',
+						<meta name="google-site-verification" content="tUI9BT1RIABRmftik1nEVColyPRItHU67IwjWOKTD74" />,
 					},
 				},
 				{
 					tag: 'meta',
 					attrs: {
-						name: 'msvalidate.01',
-						content: 'BB584AB80496A8B39F46F116A5BAF33C',
+						name: <meta name="msvalidate.01" content="BB584AB80496A8B39F46F116A5BAF33C" />,
 					},
 				},
 			],
